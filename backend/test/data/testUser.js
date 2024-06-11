12467b6fa102ee5732f3b785482836321a9f7399
Charlie Tait <charlietait147@gmail.com>
2024-06-11 16:43:59 +0100

    add user test data and test folder structure

---
+++ backend/test/data/testUser.js
@@ -0,0 +1,35 @@
+import bcrypt from 'bcrypt';
+
+const userData = {
+    userDataToImport: [
+        {
+            _id: '60f1b6b5f3f9e4f4b8f3b3b1',
+            email: 'testUser@gmail.com',
+            password: bcrypt.hashSync('testPassword123', 10),
+            reviews: []
+        },
+        {
+            _id: '60f1b6b5f3f9e4f4b8f3b3b2',
+            email: 'testUser2@gmail.com',
+            password: bcrypt.hashSync('testPassword234', 10),
+            reviews: []
+        }
+    ],
+    wellFormedUser: {
+        email: 'testUser3@gmail.com',
+        password: 'testPassword123'
+    },
+    userNoEmail: {
+        password: 'testPassword123'
+    },
+    userWrongTypeEmail: {
+        email: 123,
+        password: 'testPassword123'
+    },
+    userShortPassword: {
+        email: 'testUser4@gmail.com',
+        password: 'test'
+    },
+}
+
+export default userData;
