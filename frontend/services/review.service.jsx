9b5eb6c6d5a592186dbee6cf284294f25a8e521a
Charlie Tait <charlietait147@gmail.com>
2024-06-24 16:32:19 +0100

    add frontend review services

---
+++ frontend/services/review.service.jsx
@@ -0,0 +1,71 @@
+import axios from "axios";
+
+const API_URL = process.env.NEXT_API_URL;
+
+export const addReview = async (cafeId, name, description) => {
+    try {
+        const token = localStorage.getItem("token");
+    
+        if (!token) {
+        throw new Error("No token found. Please log in.");
+        }
+    
+        const res = await axios.post(
+        `${API_URL}/review/${cafeId}/add-review`,
+        { name, description },
+        {
+            headers: {
+            Authorization: `Bearer ${token}`,
+            },
+        }
+        );
+        console.log("Review added successfully", res.data);
+    } catch (error) {
+        console.error("Review addition failed", error);
+    }
+    }
+
+    export const editReview = async (reviewId, name, description) => {
+        try {
+            const token = localStorage.getItem("token");
+        
+            if (!token) {
+            throw new Error("No token found. Please log in.");
+            }
+        
+            const res = await axios.put(
+            `${API_URL}/review/edit-review${reviewId}`,
+            { name, description },
+            {
+                headers: {
+                Authorization: `Bearer ${token}`,
+                },
+            }
+            );
+            console.log("Review edited successfully", res.data);
+        } catch (error) {
+            console.error("Review edit failed", error);
+        }
+        }
+
+    export const deleteReview = async (reviewId) => {
+        try {
+            const token = localStorage.getItem("token");
+        
+            if (!token) {
+            throw new Error("No token found. Please log in.");
+            }
+        
+            const res = await axios.delete(
+            `${API_URL}/review/delete-review/${reviewId}`,
+            {
+                headers: {
+                Authorization: `Bearer ${token}`,
+                },
+            }
+            );
+            console.log("Review deleted successfully", res.data);
+        } catch (error) {
+            console.error("Review deletion failed", error);
+        }
+        }
