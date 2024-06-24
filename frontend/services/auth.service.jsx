8ace185ad98c5dfff60e6c27c59796b1de0807af
Charlie Tait <charlietait147@gmail.com>
2024-06-24 15:45:11 +0100

    add auth services to backend in services folder, tweak git ignore to ignore env folders

---
+++ frontend/services/auth.service.jsx
@@ -0,0 +1,76 @@
+import axios from "axios";
+
+const API_URL = process.env.NEXT_API_URL;
+
+export const register = async (email, password) => {
+  try {
+    const res = await axios.post(`${API_URL}/user/register`, {
+      email,
+      password,
+    });
+
+    const data = await res.data;
+
+    if (res.status === 201) {
+      localStorage.setItem("token", data.token);
+      console.log("User registered successfully", data);
+    } else {
+      console.error("Registration failed", data);
+    }
+  } catch (error) {
+    console.error("Registration failed", error);
+  }
+};
+
+export const login = async (email, password) => {
+  try {
+    const res = await axios.post(`${API_URL}/user/login`, {
+      email,
+      password,
+    });
+
+    const data = await res.data;
+
+    if (res.status === 201) {
+      localStorage.setItem("token", data.token);
+      console.log("User logged in successfully", data);
+    } else {
+      console.error("Login failed", data);
+    }
+  } catch (error) {
+    if (error.response && error.response.status === 400) {
+      return error.message ? error : new Error("This user couldn't be found"); // Throw the specific error message
+    } else {
+      return error.message
+        ? error
+        : new Error("An error occurred during login"); // Throw a generic error message
+    }
+  }
+};
+
+export const updatePassword = async (newPassword) => {
+    try {
+        const token = localStorage.getItem("token");
+    
+        const res = await axios.patch(
+        `${API_URL}/user/update-password`,
+        { newPassword },
+        {
+            headers: {
+            Authorization: `Bearer ${token}`,
+            },
+        }
+        );
+    
+        const data = await res.data;
+    
+        if (res.status === 200) {
+        console.log("Password updated successfully", data);
+        } else {
+        console.error("Password update failed", data);
+        }
+    } catch (error) {
+        console.error("Password update failed", error);
+    }
+    }
+
