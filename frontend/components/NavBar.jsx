023947ee2d0c18c9c0d9f26b75e6830c72e87f7d
Charlie Tait <charlietait147@gmail.com>
2024-06-28 12:04:08 +0100

    remove profile and create flyout menu from burger menu with styling

--- frontend/components/NavBar.jsx
+++ frontend/components/NavBar.jsx
@@ -1,18 +1,12 @@
 "use client";
 import { useState, useEffect } from "react";
+
 function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isProfileOpen, setIsProfileOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
-    setIsProfileOpen(false);
-  };
-
-  const toggleProfile = () => {
-    setIsProfileOpen(!isProfileOpen);
-    setIsMenuOpen(false);
   };
 
   useEffect(() => {
@@ -35,45 +29,104 @@ function NavBar() {
           <div className="h-1 w-full bg-white"></div>
           <div className="h-1 w-full bg-white"></div>
         </div>
-        {isMenuOpen && (
-          <ul className="text-center absolute top-10 right-4 z-10 bg-orange-500 shadow-lg px-2 border border-t-white mt-2 transition transform ease-in-out duration-300">
-            <li className="text-white py-2">DISCOVER</li>
-          </ul>
-        )}
       </div>
-      <div className="profile-menu" onClick={toggleProfile}>
-        <svg
-          className="w-6 h-6 text-white "
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
-            clipRule="evenodd"
-          />
-        </svg>
-        {isProfileOpen && (
-          <ul className="text-center absolute top-10 right-4 z-10 bg-orange-500 shadow-lg px-2 border border-t-white mt-2 transition transform ease-in-out duration-300">
-            {!isLoggedIn ? (
-              <>
-                <li className="text-white py-2">LOG IN</li>
-                <li className="text-white py-2">REGISTER</li>
-              </>
-            ) : (
-              <>
-                <li className="text-white py-2" onClick={handleSignOut}>
-                  MY ACCOUNT
-                </li>
-                <li className="text-white py-2" onClick={handleSignOut}>
-                  SIGN OUT
-                </li>
-              </>
-            )}
-          </ul>
-        )}
-      </div>
+      {isMenuOpen && (
+        <div className="fixed inset-0 z-40">
+          {/* Overlay */}
+          <div
+            className="fixed inset-0 bg-black bg-opacity-50 transition-opacity duration-300 backdrop-blur-sm"
+            onClick={toggleMenu} // Close the menu when clicking on the overlay
+          ></div>
+
+          {/* Flyout Menu */}
+          <div className="fixed top-0 right-0 z-50 h-screen p-5 overflow-y-auto bg-white w-64 dark:bg-gray-800 transition-transform transform duration-300 ease-in-out shadow-2xl">
+            <button
+              type="button"
+              aria-controls="drawer-navigation"
+              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-5 right-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+              onClick={toggleMenu}
+            >
+              <svg
+                className="w-3 h-3"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7L1 13"
+                />
+              </svg>
+              <span className="sr-only">Close menu</span>
+            </button>
+            <h1 className="text-2xl font-bold pt-4">Swish .</h1>
+            <div className="border border-t-gray-300 mt-4"></div>
+            <ul className="mt-6 space-y-5 text-center">
+              <li className="text-white py-2 bg-orange-500 hover:bg-orange-400 shadow-lg">
+                Discover
+              </li>
+              <li className="text-white py-2 bg-orange-500 hover:bg-orange-400 shadow-lg">
+                Contact Us
+              </li>
+              <div className="border border-t-gray-300 mt-6"></div>
+              {!isLoggedIn ? (
+                <>
+                  <li className="text-white py-2 bg-gray-800  hover:bg-gray-600  shadow-lg flex items-center justify-center">
+                    Sign In
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-5 h-5 ml-2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                      />
+                    </svg>
+                  </li>
+                  <li className="text-white py-2 flex items-center justify-center bg-gray-800 hover:bg-gray-600 shadow-lg">
+                    Register
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-5 h-5 ml-2"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                      />
+                    </svg>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="text-white py-2 bg-gray-800  hover:bg-gray-600  shadow-lg">
+                    My Account
+                  </li>
+                  <li
+                    className="text-white py-2 bg-gray-800  hover:bg-gray-600  shadow-lg"
+                    onClick={handleSignOut}
+                  >
+                    Sign Out
+                  </li>
+                </>
+              )}
+            </ul>
+          </div>
+        </div>
+      )}
     </>
   );
 }
