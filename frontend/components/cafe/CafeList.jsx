5a1896699adad4dd04979edb840d0f4a4c8b8a37
Charlie Tait <charlietait147@gmail.com>
2024-07-01 14:18:29 +0100

    sort cafes into alphabetical order

--- frontend/components/cafe/CafeList.jsx
+++ frontend/components/cafe/CafeList.jsx
@@ -1,9 +1,10 @@
 import CafeCard from "./CafeCard";
 
 function CafeList({ cafes }) {
+    const sortedCafes = cafes.sort((a, b) => a.name.localeCompare(b.name));
   return (
     <div className="flex flex-col gap-5 px-4 pb-4">
-      {cafes.map((cafe) => (
+      {sortedCafes.map((cafe) => (
         <CafeCard key={cafe._id} cafe={cafe} />
       ))}
     </div>
