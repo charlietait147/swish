4f026def6d324b2420fb158df36eb8f3d640c000
Charlie Tait <charlietait147@gmail.com>
2024-07-04 14:14:02 +0100

    add API call to fetch single cafe in cafe details page, add header and footer to page

--- frontend/app/cafe/[cafeId]/page.jsx
+++ frontend/app/cafe/[cafeId]/page.jsx
@@ -1,5 +1,38 @@
+"use client";
+
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import { fetchCafe } from "@/services/cafe.service";
+import { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+
 export default function CafePage() {
-    return (
-        <div></div>
-    );
+  const [cafe, setCafe] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const { cafeId } = useParams();
+
+  const getCafe = async () => {
+    if (cafeId) {
+      try {
+        const response = await fetchCafe(cafeId);
+        setCafe(response);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
     }
+  };
+
+  useEffect(() => {
+    getCafe();
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
