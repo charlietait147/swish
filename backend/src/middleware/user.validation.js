f6d4662395d441ec3af2019272674cd36a819348
Charlie Tait <charlietait147@gmail.com>
2024-07-14 18:34:39 +0100

    tweak validation to use full stop and exclamation mark and change validation message

--- backend/src/middleware/user.validation.js
+++ backend/src/middleware/user.validation.js
@@ -7,8 +7,8 @@ export const userRegisterValidation = [
     check('password')
         .exists()
         .isString()
-        .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/)
-        .withMessage('Password must be between 8 to 16 characters long and must contain at least one letter and one number')
+        .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!.\d]{6,}$/)
+        .withMessage('Password must be at least 6 characters long and must contain at least one letter and one number')
 ]
 
 export const updatePasswordValidation = [
