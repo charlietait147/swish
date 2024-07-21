e3d2c7b2eec4023a5c80ee02c165f50dbb2c3721
Charlie Tait <charlietait147@gmail.com>
2024-07-21 17:27:32 +0100

    add hero image and descriptionwith styling and breakpoint

--- frontend/components/Hero.jsx
+++ frontend/components/Hero.jsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
+import description from "../public/images/swish-description.jpeg";
+import hero from "../public/images/hero-image.jpg";
 function Hero() {
-    return ( 
-        <div></div>
-     );
+  return (
+    <div>
+      <div className="md:flex flex-row">
+        <Image
+          src={hero}
+          alt="Four coffees clinking"
+          className="relative md:w-1/2"
+        />
+        <h2 className="absolute top-32 left-8 text-white font-bold text-2xl">Swish .</h2>
+        <div className="pt-6 pb-4 px-4 md:flex flex-col justify-center md:px-8">
+          <div className=" border border-gray-200 shadow-lg">
+            <Image src={description} alt="Google made swish description" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
