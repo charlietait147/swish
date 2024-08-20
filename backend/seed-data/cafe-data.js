f000f2ed340e5b65c981d2fd2afd6331e51a4d78
Charlie Tait <charlietait147@gmail.com>
2024-08-20 10:20:35 +0100

    update cafe data with more cafes

--- backend/seed-data/cafe-data.js
+++ backend/seed-data/cafe-data.js
@@ -165,7 +165,7 @@ const cafes = [
     {
         name: 'The Refectory',
         location: 'Winchester',
-        description:'The light and airy Refectory offers a fully stocked licensed bar, plenty of space for dancing, a pretty walled garden and amazing cathedral views.  Located in the heart of Winchester and adjacent to the Cathedral itself, the venue is the perfect food or coffee stop for those exploring the historic city.   Why not enjoy a stroll around the old town or Cathedral grounds before or after your visit?',
+        description: 'The light and airy Refectory offers a fully stocked licensed bar, plenty of space for dancing, a pretty walled garden and amazing cathedral views.  Located in the heart of Winchester and adjacent to the Cathedral itself, the venue is the perfect food or coffee stop for those exploring the historic city.   Why not enjoy a stroll around the old town or Cathedral grounds before or after your visit?',
         website: 'https://www.winchester-cathedral.org.uk/welcome/cafe/',
         image: 'the-refectory-cafe-no-border.jpeg',
         borderImage: 'the-refectory-cafe.jpeg',
@@ -188,10 +188,10 @@ const cafes = [
         reviews: []
     },
     {
-        name:'Heidi',
-        location:'Haslemere',
+        name: 'Heidi',
+        location: 'Haslemere',
         description: 'Heidi is a family-run cafe in the heart of Haslemere. The cafe is open seven days a week and serves a range of delicious food and drinks. Heidi is the perfect place to relax and enjoy a coffee or a light lunch. The cafe is also available for private hire and is the perfect venue for a special occasion.',
-        website:'https://www.heidibakery.co.uk/locations/heidi-haslemere/',
+        website: 'https://www.heidibakery.co.uk/locations/heidi-haslemere/',
         image: '/',
         borderImage: '/',
         lat: 51.08834,
@@ -229,6 +229,268 @@ const cafes = [
             {
                 type: 'Onsite Parking',
                 url: 'onsite-parking.jpeg',
+            }
+        ],
+        reviews: []
+    },
+    {
+        name: 'Edison\'s Coffee and Wine',
+        location: 'Southampton',
+        description: 'Located near the High Street and within the Old Town in Southampton, Edison\'s offer freshly baked goods from a local supplier and a quality in coffee that\'s not always easy to come by in the chain-dominated city centre. Its high ceilings and bright interior create a spacious and versatile environment perfect for socialising over a coffee or small lunch. The location is perfect for visiting the Old Town or West Quay shopping centre.',
+        website: 'https://edisons.life/',
+        image: 'edisons-no-border.jpeg',
+        borderImage: 'edisons.jpeg',
+        lat: 50.89943,
+        lng: -1.40396,
+        icons: [
+            {
+                type: 'Accessible by Public Transport',
+                url: 'accessible-by-transport.jpeg'
+            },
+            {
+                type: 'Dog Friendly',
+                url: 'dog-friendly.jpeg',
+            },
+            {
+                type: 'Takes Bookings',
+                url: 'bookings.jpeg',
+            }
+        ],
+        reviews: []
+    },
+    {
+        name: 'Coffee Diem',
+        location: 'Farnham',
+        description: 'Found in one of Farnham\'s most popular shopping streets, Coffee Diem is an Italian-themed coffee and lunch bar serving a wide selection of coffees, teas, shakes, soups, panini sandwiches, and homemade Italian pizza. During the warmer months ample seating is available outside, providing an alfresco dining experience for those who enjoy the hustle and bustle of a picturesque market town. Nearby attractions to consider afterwards include Farnham Castle and Frensham Great Pond, situated just south of the Surrey town.',
+        website: 'https://coffeediemfarnham.co.uk/',
+        image: 'coffee-diem-no-border.jpeg',
+        borderImage: 'coffee-diem.jpeg',
+        lat: 51.21482,
+        lng: -0.80134,
+        icons: [
+            {
+                type: 'Accessible by Public Transport',
+                url: 'accessible-by-transport.jpeg'
+            },
+            {
+                type: 'Dog Friendly',
+                url: 'dog-friendly.jpeg',
+            },
+            {
+                type: 'Hot Food Available',
+                url: 'hot-food.jpeg',
+            },
+            {
+                type: 'Takes Bookings',
+                url: 'bookings.jpeg',
+            }
+        ], 
+        reviews: []
+    },
+    {
+        name: 'Hemingways',
+        location: 'Haslemere',
+        website: 'https://hemingwaysofhaslemere.com/',
+        description: 'Located in the heart of Haslemere, Hemingways is a quirky coffee shop perfect for those looking for a breakfast or brunch treat. An extensive menu is available offering breakfast, lunch, snacks and afternoon tea. The venue is a very short and scenic drive for those visiting the Devil\'s Punchbowl; why not start the morning here?',
+        image: 'hemingways-no-border.jpeg',
+        borderImage: 'hemingways.jpeg',
+        lat: 51.08875,
+        lng: -0.70867,
+        icons: [
+            {
+                type: 'Accessible by Public Transport',
+                url: 'accessible-by-transport.jpeg'
+            },
+            {
+                type: 'Dog Friendly',
+                url: 'dog-friendly.jpeg',
+            },
+            {
+                type: 'Hot Food Available',
+                url: 'hot-food.jpeg',
+            },
+            {
+                type: 'Takes Bookings',
+                url: 'bookings.jpeg',
+            }
+        ],
+        reviews: []
+    },
+    {
+        name: 'Harris and Hoole',
+        location: 'Farnham',
+        website: 'https://www.harrisandhoole.co.uk/shop/farnham',
+        description: 'Positioned on the corner of Farnham\'s charming Lion & Lamb Yard, this Harris & Hoole establishment is a spacious, two-storey café serving high quality, well-presented coffee and snacks. The venue boasts four choices for seating; downstairs, outside, upstairs, and on the terrace balcony. We\'d particularly recommend this café as a good spot for a date, or somewhere to recharge after a stroll around the nearby Frensham Great Pond.',
+        image: 'harris-hoole-no-border.jpeg',
+        borderImage: 'harris-hoole.jpeg',
+        lat: 51.21444,
+        lng: -0.80080,
+        icons: [
+            {
+                type: 'Accessible by Public Transport',
+                url: 'accessible-by-transport.jpeg'
+            },
+            {
+                type: 'Dog Friendly',
+                url: 'dog-friendly.jpeg',
+            },
+            {
+                type: 'Takes Bookings',
+                url: 'bookings.jpeg',
+            }
+        ],
+        reviews: []
+    },
+    {
+        name: 'Wild and Green Kitchen',
+        location: 'Cranleigh',
+        website: 'https://wildandgreenkitchen.co.uk/',
+        description: 'Wild & Green Kitchen is an independent, artisan café serving high quality coffee and natural food. The venue is positioned with a wonderful view of Cranleigh High Street and is well worth a visit for those visiting the quaint Surrey village. During the summer season, we\'d recommend a takeaway from this venue and walking a few minutes to the Common, where cricket can sometimes be watched in peaceful surroundings.',
+        image: 'wild-green-kitchen-no-border.jpeg',
+        borderImage: 'wild-green-kitchen.jpeg',
+        lat: 51.14200,
+        lng: -0.49286,
+        icons: [
+            {
+                type: 'Accessible by Public Transport',
+                url: 'accessible-by-transport.jpeg'
+            },
+            {
+                type: 'Dog Friendly',
+                url: 'dog-friendly.jpeg',
+            },
+            {
+                type: 'Takes Bookings',
+                url: 'bookings.jpeg',
+            }
+        ],
+        reviews: []
+    },
+    {
+        name: 'Lilly\'s Tea and Coffee House',
+        location: 'Wickham',
+        website: 'https://livelifelovecake.com/lillys-tea-coffee-house-wickham-hampshire/',
+        description: 'Lilly\'s is a charming café located in the heart of Wickham on the corner of The Square. Its mismatched furniture, unusual ornaments and traditional crockery give the venue a homely feel akin to being at Granny’s. The popular venue offers a variety of savoury snacks and sandwiches as well as a modest selection of hot lunches, making it the perfect excuse to venture out to the very attractive village of Wickham.',
+        image: 'lillys-no-border.jpeg',
+        borderImage: 'lillys.jpeg',
+        lat: 50.90026,
+        lng: -1.18712,
+        icons: [
+            {
+                type: 'Dog Friendly',
+                url: 'dog-friendly.jpeg',
+            },
+            {
+                type: 'Onsite Parking',
+                url: 'onsite-parking.jpeg',
+            },
+            {
+                type: 'Hot Food Available',
+                url: 'hot-food.jpeg',
+            },
+            {
+                type: 'Takes Bookings',
+                url: 'bookings.jpeg',
+            }
+        ],
+        reviews: []
+    },
+    {
+        name: 'The Bumblebee Cafe',
+        location: 'Rowland\'s Castle',
+        website: 'https://www.thebumblebeecafe.co.uk/',
+        description: 'The Bumblebee Café is a traditional village cafe, nestled right next to the village green of Rowlands Castle. In the heart of the picturesque village, its décor is bright and perfect for enjoying a quintessentially English lunch or afternoon tea. Although the village is somewhat remote, the café is located very close to its railway station; a small stop on the London to Portsmouth main line. Why not break up the journey and pay a visit to this hidden gem?',
+        image: 'bumblebee-cafe-no-border.jpeg',
+        borderImage: 'bumblebee-cafe.jpeg',
+        lat: 50.89115,
+        lng: -0.95829,
+        icons: [
+            {
+                type: 'Accessible by Public Transport',
+                url: 'accessible-by-transport.jpeg'
+            },
+            {
+                type: 'Dog Friendly',
+                url: 'dog-friendly.jpeg',
+            },
+            {
+                type: 'Hot Food Available',
+                url: 'hot-food.jpeg',
+            },
+            {
+                type: 'Takes Bookings',
+                url: 'bookings.jpeg',
+            },
+        ],
+        reviews: []
+    },
+    {
+        name: 'The Pavilion Tea Room',
+        location: 'Rowland\'s Castle',
+        website: 'https://www.paviliontearoom.co.uk/',
+        description: 'Hidden within 1800 acres of ancient woodland on the Hampshire and West Sussex border, Stansted Park, with its stunning stately home and forest, is a wonderful setting for family and friends to gather. In the heart of The Walled Garden is a beautifully restored Victorian glass house, which now houses The Pavilion Tearoom. General bookings are not accepted but to enjoy the venue\'s esteemed afternoon tea (after 3:30pm), customers are required to book slots.',
+        image: 'pavilion-tea-room-no-border.jpeg',
+        borderImage: 'pavilion-tea-room.jpeg',
+        lat: 50.88737,
+        lng: -0.91899,
+        icons: [
+            {
+                type: 'Hot Food Available',
+                url: 'hot-food.jpeg',
+            },
+            {
+                type: 'Onsite Parking',
+                url: 'onsite-parking.jpeg',
+            },
+        ],
+        reviews: []
+    },
+    {
+        name: 'No. 60 Coffee and Wine Bar',
+        location: 'Bishop\'s Waltham',
+        website: '',
+        description: 'This intriguing coffee and wine bar is a family run, lively shop offering a continental feel, serving light breakfast, lunch and evening snacks. A wide selection of drinks are available including local artisan coffee, speciality teas and a good selection of wines and beers. The venue marks a perfect spot for those visiting or passing through the quiet Hampshire town.',
+        image: 'no-60-no-border.jpeg',
+        borderImage: 'no-60.jpeg',
+        lat: 50.95482,
+        lng: -1.21220,
+        icons: [
+            {
+                type: 'Dog Friendly',
+                url: 'dog-friendly.jpeg',
+            },
+            {
+                type: 'Takes Bookings',
+                url: 'bookings.jpeg',
+            }
+        ],
+        reviews: []
+    },
+    {
+        name: 'The Natural Food Deli',
+        location: 'Petersfield',
+        website: 'https://www.thenaturalfooddeli.co.uk/',
+        description: 'The Natural Food Deli is a health food café and restaurant to be found in the heart of the South Downs in the old town of Petersfield. It offers a range of healthy food options and caters particularly well for vegetarian and vegan customers. It is conveniently based between the town centre and Petersfield Lake – easily walkable to both – making it suitable for those visiting the market town as well as those heading out for waterside strolling or bathing. If dining in, customers can enjoy an interesting interior floorplan decorated with a vintage, rustic style.',
+        image: 'natural-food-deli-no-border.jpeg',
+        borderImage: 'natural-food-deli.jpeg',
+        lat: 51.00354,
+        lng: -0.93440,
+        icons: [
+            {
+                type: 'Accessible by Public Transport',
+                url: 'accessible-by-transport.jpeg'
+            },
+            {
+                type: 'Dog Friendly',
+                url: 'dog-friendly.jpeg',
+            },
+            {
+                type: 'Hot Food Available',
+                url: 'hot-food.jpeg',
+            },
+            {
+                type: 'Takes Bookings',
+                url: 'bookings.jpeg',
             }
         ],
         reviews: []
