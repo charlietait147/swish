a7955e17fd9419d55ae4128343e69c7f40816765
Charlie Tait <charlietait147@gmail.com>
2024-08-28 17:10:06 +0100

    installjest, testing library react and jest dom, add set up test file and add scripts for testing

---
+++ frontend/test/config/setup.js
@@ -0,0 +1,11 @@
+// Import the necessary modules from Jest and React Testing Library
+import { afterEach } from '@jest/globals';
+import { cleanup } from '@testing-library/react';
+
+// Import matchers from jest-dom
+import '@testing-library/jest-dom/extend-expect';
+
+// Run cleanup after each test case (to remove any elements from the DOM)
+afterEach(() => {
+  cleanup();
+});
