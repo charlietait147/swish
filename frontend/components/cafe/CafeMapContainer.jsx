15cb141242d09a557d643b0262637b319e1c2bd3
Charlie Tait <charlietait147@gmail.com>
2024-09-29 13:08:35 +0100

    add address to cafe map container component and format styling at different breakpoints

--- frontend/components/cafe/CafeMapContainer.jsx
+++ frontend/components/cafe/CafeMapContainer.jsx
@@ -1,5 +1,6 @@
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 import { useMemo } from "react";
+import Link from "next/link";
 
 function CafeMapContainer({ cafe }) {
   const { isLoaded } = useLoadScript({
@@ -14,7 +15,40 @@ function CafeMapContainer({ cafe }) {
   );
   return (
     <div className="max-w-screen-lg mx-auto xs:px-6 sm:px-10 lg:pt-6 xl:px-0 px-4 flex flex-col gap-3 pb-6">
-      <h1 className="text-xl font-semibold">Location</h1>
+      <div className="flex flex-col lg:flex-row lg:items-center lg:gap-5 lg:mb-2">
+        <h1 className="text-xl font-semibold">Location</h1>
+        <div className="flex flex-row gap-2 my-2 lg:my-0 lg:items-center lg:gap-1">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+            />
+          </svg>
+          <Link
+            href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+              cafe.address
+            )}`}
+            className="text-gray-800 text-sm hover:underline lg:mt-0.5"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {cafe.address}
+          </Link>
+        </div>
+      </div>
       <div className="map">
         {!isLoaded ? (
           <h1>Loading...</h1>
@@ -39,4 +73,3 @@ function CafeMapContainer({ cafe }) {
 }
 
 export default CafeMapContainer;
-
