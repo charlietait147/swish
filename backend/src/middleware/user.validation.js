1b44b2060322db3876a77a2fa666afed061f322f
Charlie Tait <charlietait147@gmail.com>
2026-04-04 16:11:51 +0100

    update validation for password

--- backend/src/middleware/user.validation.js
+++ backend/src/middleware/user.validation.js
@@ -7,25 +7,25 @@ export const userRegisterValidation = [
     check('password')
         .exists()
         .isString()
-        .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!.\d]{6,}$/)
-        .withMessage('Password must be at least 6 characters long and must contain at least one letter and one number')
+        .matches(/^(?=.*[A-Za-z])(?=.*\d).{8,}$/)
+        .withMessage('Password must be at least 8 characters long and must contain at least one letter and one number')
 ]
 
 export const updatePasswordValidation = [
     check('newPassword')
         .exists()
-        .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/)
-        .withMessage('New password must be between 8 to 16 characters long and must contain at least one letter and one number')
+        .matches(/^(?=.*[A-Za-z])(?=.*\d).{8,}$/)
+        .withMessage('New password must be at least 8 characters and must contain at least one letter and one number')
 ];
 
 export const resetPasswordValidation = [
-    check('token')
-      .exists()
-      .notEmpty()
-      .withMessage('Reset token is required'),
+    // check('token')
+    //   .exists()
+    //   .notEmpty()
+    //   .withMessage('Reset token is required'),
     
     check('newPassword')
       .exists()
-      .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/)
-      .withMessage('New password must be between 8 to 16 characters long and must contain at least one letter and one number')
+      .matches(/^(?=.*[A-Za-z])(?=.*\d).{8,}$/)
+      .withMessage('New password must be at least 8 characters and must contain at least one letter and one number')
   ];
